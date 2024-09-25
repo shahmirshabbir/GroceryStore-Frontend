813c300ec1389be8a7f183f9e4960fab0d396d86
shahmir <shabirshahmir@gmail.com>
2024-09-25 23:56:35 +0500

    push new project

---
+++ assets/js/test.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('sidebar-active-links');
+
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+  document.body.style.backgroundColor = '#f6f9ff';
+
+  
+});
